058080a9dab1881245b8d71b62bc3f2818ffa3ee
view <view@views-MacBook-Pro.local>
2026-06-18 11:12:12 -0700

    make a header

---
+++ script/home-page.js
@@ -0,0 +1,5 @@
+const url =
+  "https://api.themoviedb.org/3/discover/movie?api_key=cb8d9a517e7387524c6cd936f1752bc0";
+const mainUrlImage = "https://image.tmdb.org/t/p/original";
+
+const containerPoster = document.querySelector(".container-poster");
